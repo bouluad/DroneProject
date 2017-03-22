539fbac5da975e085c0ae9764c4bcfed63025311
SalmaBouri <bouri.salma@gmail.com>
2017-03-22 08:45:37 +0100

    new intervention codis

---
+++ app/src/main/java/istic/fr/droneproject/NewInterventionActivity.java
@@ -0,0 +1,18 @@
+package istic.fr.droneproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by salma on 21/03/17.
+ */
+
+public class NewInterventionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_intervention);
+    }
+}
