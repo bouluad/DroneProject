aecf7f1253aeeaf1098c3bd9e17818a91acd51fc
yousra <yousraelghzizal@hotmail.fr>
2017-03-22 01:28:03 +0100

    add drone service API Rest

---
+++ app/src/main/java/istic/fr/droneproject/service/retrofit/DroneService.java
@@ -0,0 +1,25 @@
+package istic.fr.droneproject.service.retrofit;
+import istic.fr.droneproject.model.Position;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+
+/**
+ * Created by yousra on 22/03/17.
+ */
+
+
+public interface DroneService {
+    public static final String ENDPOINT = "http://148.60.11.238:27018";
+
+    @Headers({ "Accept: application/json" })
+    @GET("/position")
+    Call<Position> getPosition();
+
+    @POST("/position")
+    Call<Position> setPosition(@Body Position position);
+
+}
