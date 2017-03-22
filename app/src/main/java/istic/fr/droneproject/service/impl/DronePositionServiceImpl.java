375d87e8739271fe3d711cd9283c8a5100756ce0
yousra <yousraelghzizal@hotmail.fr>
2017-03-22 01:37:07 +0100

    add dronePositionService Implementation

---
+++ app/src/main/java/istic/fr/droneproject/service/impl/DronePositionServiceImpl.java
@@ -0,0 +1,67 @@
+package istic.fr.droneproject.service.impl;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import istic.fr.droneproject.model.Position;
+import istic.fr.droneproject.service.retrofit.DroneService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by yousra on 22/03/17.
+ */
+
+public class DronePositionServiceImpl {
+
+    
+    private void getDronePositionwithRetrofit()  {
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(DroneService.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        DroneService droneService  = retrofit.create(DroneService.class);
+        Call<Position> position= droneService.getPosition();
+        position.enqueue(new Callback<Position>() {
+            @Override
+            public void onResponse(Call<Position> call, Response<Position> response) {
+                Position pos=response.body();
+              //  Log.e("getPosition======>",pos.latitude+""+pos.longitude);
+
+            }
+
+            @Override
+            public void onFailure(Call<Position> call, Throwable t) {
+                //Handle failure
+                Log.e("EROOOOOOOOOR======>","you can't retreive drone position");
+            }
+        });
+
+    }
+    private void setDronePositionwithRetrofit(Position pos)  {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(DroneService.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        DroneService droneService  = retrofit.create(DroneService.class);
+        Call<Position> position= droneService.setPosition(pos);
+        position.enqueue(new Callback<Position>() {
+            @Override
+            public void onResponse(Call<Position> call, Response<Position> response) {
+                Position pos=response.body();
+                //Log.e("getPosition======>",pos.getPosition().get(0)+""+pos.getPosition().get(1));
+
+            }
+
+            @Override
+            public void onFailure(Call<Position> call, Throwable t) {
+                //Handle failure
+                Log.e("EROOOOOOOOOR======>","you can't set drone position");
+            }
+        });
+
+    }
+}
