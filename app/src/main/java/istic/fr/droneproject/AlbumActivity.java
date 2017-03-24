92de771d269c9f0baa135d6ff6ebf18a5d3e7eb4
Guillaume Chopin <guillaume.chopin@etudiant.univ-rennes1.fr>
2017-03-24 18:02:33 +0100

    Album photo

--- app/src/main/java/istic/fr/droneproject/AlbumActivity.java
+++ app/src/main/java/istic/fr/droneproject/AlbumActivity.java
@@ -1,18 +1,80 @@
 package istic.fr.droneproject;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import istic.fr.droneproject.adapter.AlbumPhotoAdapter;
+import istic.fr.droneproject.model.Intervention;
+import istic.fr.droneproject.model.Photo;
+import istic.fr.droneproject.service.impl.InterventionServiceCentral;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by bouluad on 22/03/17.
  */
 public class AlbumActivity extends android.support.v4.app.Fragment {
 
+    private static final String ARG_ID = "idIntervention";
+    private String idIntervention;
+
+    public AlbumActivity() {
+        //Required empty constructor
+    }
+
+    public static AlbumActivity newInstance(String idIntervention) {
+        AlbumActivity fragment = new AlbumActivity();
+        Bundle args = new Bundle();
+        args.putString(ARG_ID, idIntervention);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            idIntervention = getArguments().getString(ARG_ID);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.album_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerViewPhotos = (RecyclerView) view.findViewById(R.id.album_liste);
+
+        recyclerViewPhotos.setLayoutManager(new LinearLayoutManager(getContext()));
+        final List<Photo> photos = new ArrayList<>();
+        final AlbumPhotoAdapter adapter = new AlbumPhotoAdapter(photos, R.layout.album_photo_item, getContext());
+        recyclerViewPhotos.setAdapter(adapter);
+
+        InterventionServiceCentral.getInstance().getInterventionById(idIntervention, new Callback<Intervention>() {
+            @Override
+            public void onResponse(Call<Intervention> call, Response<Intervention> response) {
+                photos.clear();
+                photos.addAll(response.body().photos);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<Intervention> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/istic/fr/droneproject/UserMainActivity.java
+++ app/src/main/java/istic/fr/droneproject/UserMainActivity.java
@@ -1,16 +1,13 @@
 package istic.fr.droneproject;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 /**
  * Created by bouluad on 22/03/17.
@@ -31,15 +28,19 @@ public class UserMainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private String idIntervention;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_main_activity);
 
+        Intent intent = getIntent();
+        idIntervention = intent.getStringExtra("idIntervention");
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager(), this.idIntervention);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
