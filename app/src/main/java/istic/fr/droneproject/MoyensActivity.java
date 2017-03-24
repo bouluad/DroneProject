bd80a4c0429f071c93ca3ce42a4ea5c9d7497c48
bouluad <med.bouluad@gmail.com>
2017-03-24 15:40:04 +0100

    test composant textview

--- app/src/main/java/istic/fr/droneproject/MoyensActivity.java
+++ app/src/main/java/istic/fr/droneproject/MoyensActivity.java
@@ -1,10 +1,10 @@
 package istic.fr.droneproject;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by bouluad on 22/03/17.
@@ -13,6 +13,9 @@ public class MoyensActivity extends android.support.v4.app.Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.moyens_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.moyens_fragment, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.textViewTestMoyens);
+        textView.setText("Juste Test");
+        return rootView;
     }
 }
