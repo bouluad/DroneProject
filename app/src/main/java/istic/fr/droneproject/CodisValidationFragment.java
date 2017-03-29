7c7163be7fcba1467b94bc539409ede9f9458dd0
KimAndGumi <nirina.ranaivoson@gmail.com>
2017-03-29 18:02:50 +0200

    popup de confirmation de validation (CODIS)

--- app/src/main/java/istic/fr/droneproject/CodisValidationFragment.java
+++ app/src/main/java/istic/fr/droneproject/CodisValidationFragment.java
@@ -3,17 +3,21 @@ package istic.fr.droneproject;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import istic.fr.droneproject.adapter.ValidationRecyclerAdapter;
 import istic.fr.droneproject.model.EtatVehicule;
@@ -26,14 +30,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/**
- * Created by nirina on 27/03/17.
- */
-
 public class CodisValidationFragment extends Fragment {
 
     List<Validation> validations;
     ValidationRecyclerAdapter adapter;
+    EtatVehicule tmpEtat;
+    Validation tmpValidation;
+
     public CodisValidationFragment(){
 
     }
@@ -44,7 +47,7 @@ public class CodisValidationFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedStateInstance){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedStateInstance){
         View validationView = inflater.inflate(R.layout.codis_validation, container, false);
 
         final RecyclerView validationsRecycler = (RecyclerView) validationView.findViewById(R.id.codis_list_validation);
@@ -55,55 +58,25 @@ public class CodisValidationFragment extends Fragment {
             @Override
             public void clickValidation(Validation validation) {
                 if (validation.vehicule.position == null){
-                    validation.vehicule.etat = EtatVehicule.PARKING;
+                    tmpEtat = EtatVehicule.PARKING;
                 }else{
-                    validation.vehicule.etat = EtatVehicule.ENGAGE;
+                    tmpEtat = EtatVehicule.ENGAGE;
                 }
-
-                SimpleDateFormat formatter = new SimpleDateFormat("HH/mm");
-                validation.vehicule.heureEngagement = formatter.format(new Date());
-
-                InterventionService service = InterventionServiceCentral.getInstance();
-                service.updateIntervention(validation.intervention, new Callback<Void>() {
-                    @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
-                        chargerValidations();
-                        Log.e("TestCodisValidationActi","modification OK");
-                    }
-
-                    @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
-                        Log.e("TestCodisValidationActi","modification KO");
-                    }
-                });
+                tmpValidation = validation;
+                confirmerPopUp(savedStateInstance);
             }
         };
         ValidationRecyclerAdapter.RefusClickListener refusListener = new ValidationRecyclerAdapter.RefusClickListener() {
             @Override
             public void clickRefus(Validation validation) {
-                validation.vehicule.etat = EtatVehicule.ANNULE;
-                InterventionService service = InterventionServiceCentral.getInstance();
-                service.updateIntervention(validation.intervention, new Callback<Void>() {
-                    @Override
-                    public void onResponse(Call<Void> call, Response<Void> response) {
-                        chargerValidations();
-                        Log.e("TestCodisValidationActi","modification OK");
-                    }
-
-                    @Override
-                    public void onFailure(Call<Void> call, Throwable t) {
-                        Log.e("TestCodisValidationActi","modification KO");
-                    }
-                });
+                tmpEtat = EtatVehicule.ANNULE;
+                tmpValidation = validation;
+                confirmerPopUp(savedStateInstance);
             }
         };
-
-        //final ValidationRecyclerAdapter adapter = new ValidationRecyclerAdapter(validations,R.layout.codis_validation_item,refusListener,validListener);
         adapter = new ValidationRecyclerAdapter(validations,R.layout.codis_validation_item,refusListener,validListener);
         validationsRecycler.setAdapter(adapter);
-
         chargerValidations();
-        //adapter.notifyDataSetChanged();
         return validationView;
     }
 
@@ -116,33 +89,62 @@ public class CodisValidationFragment extends Fragment {
                 interventions.clear();
                 interventions.addAll(response.body());
                 validations.clear();
-
-                //Integer count = 0;
-
                 for (Intervention i:interventions){
                     if (i.vehicules!= null) {
                         for (Vehicule v : i.vehicules) {
                             if (v.etat == EtatVehicule.DEMANDE) {
-                                /*count+=1;
-                                Log.e("Check objet",count.toString());
-                                Log.e("Check objet",v.etat.toString());
-                                Log.e("Check objet",v.heureDemande);
-                                Log.e("Check objet",v.nom);
-                                Log.e("Check objet",i.libelle);*/
                                 validations.add(new Validation(v, i));
                             }
                         }
                     }
                 }
-                Log.e("CodisValidationActivi","Nombre de véhicules à valider : "+validations.size());
+                Log.e("CodisValidationFragment","Nombre de véhicules à valider : "+validations.size());
                 adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onFailure(Call<List<Intervention>> call, Throwable t) {
                 //DO NOTHING
-                Log.e("CodisValidationActivi", t.toString());
+                Log.e("CodisValidationFragment", t.toString());
             }
         });
+    }
+
+    public void confirmerPopUp(Bundle savedInstanceState) {
+        AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(getContext());
+        confirmBuilder.setTitle("Confirmation");
+        LayoutInflater inflater = getLayoutInflater(savedInstanceState);
+        final View popupLayout = inflater.inflate(R.layout.codis_validation_popup, null);
+        confirmBuilder.setView(popupLayout);
+        final AlertDialog alert = confirmBuilder.create();
+        TextView confirmerText = (TextView) popupLayout.findViewById(R.id.codis_validation_text);
+        if (tmpEtat.equals(EtatVehicule.ANNULE)){
+            confirmerText.setText("Refuser ?");
+        }else{
+            confirmerText.setText("Accepter ?");
+        }
+        Button confirmerButton = (Button) popupLayout.findViewById(R.id.codis_validation_button);
+        confirmerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tmpValidation.vehicule.etat = tmpEtat;
+                tmpValidation.vehicule.heureEngagement = new SimpleDateFormat("HH:mm", Locale.FRANCE).format(new Date());
+                InterventionService service = InterventionServiceCentral.getInstance();
+                service.updateIntervention(tmpValidation.intervention, new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        alert.dismiss();
+                        chargerValidations();
+                        Log.e("TestCodisValidationActi","modification OK");
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Log.e("TestCodisValidationActi","modification KO");
+                    }
+                });
+            }
+        });
+        alert.show();
     }
 }
