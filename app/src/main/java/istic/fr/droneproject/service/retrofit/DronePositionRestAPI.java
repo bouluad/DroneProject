6a5933ee909776e734337602db0d6d0d7e13d834
yousra <yousraelghzizal@hotmail.fr>
2017-04-24 17:56:34 +0200

    Adding service for DronePosition

--- app/src/main/java/istic/fr/droneproject/service/retrofit/DronePositionRestAPI.java
+++ app/src/main/java/istic/fr/droneproject/service/retrofit/DronePositionRestAPI.java
@@ -1,6 +1,7 @@
 package istic.fr.droneproject.service.retrofit;
 
 import istic.fr.droneproject.model.Drone;
+import istic.fr.droneproject.model.DronePosition;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -8,22 +9,18 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
-
 /**
- * Created by yousra on 22/03/17.
+ * Created by yousra on 24/04/17.
  */
 
-
-public interface DroneRestAPI {
-     String ENDPOINT = "http://148.60.11.238:8080/";
+public interface DronePositionRestAPI {
+    String ENDPOINT = "http://148.60.11.238:8080/";
 
     @Headers({ "Accept: application/json" })
-    @GET("/drones/{id}/intervention")
-    Call<Drone> getDroneById(@Path("id") String id);
+    @GET("/dronePosition/{id}/intervention")
+    Call<DronePosition> getDronePositionByIdIntervention(@Path("id") String id);
 
 
-    @POST("/drones")
-    Call<Void> setDrone(@Body Drone drone );
 
 
 }
