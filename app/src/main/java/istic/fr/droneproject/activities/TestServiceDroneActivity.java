f8f29667133d1f9e3703d3244c7cc19a5cf74caa
yousra <yousraelghzizal@hotmail.fr>
2017-04-25 08:58:30 +0200

    Modification of TestServices

--- app/src/main/java/istic/fr/droneproject/activities/TestServiceDroneActivity.java
+++ app/src/main/java/istic/fr/droneproject/activities/TestServiceDroneActivity.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 import istic.fr.droneproject.R;
 import istic.fr.droneproject.model.Drone;
+import istic.fr.droneproject.model.EtatDrone;
 import istic.fr.droneproject.model.Intervention;
-import istic.fr.droneproject.service.DroneService;
-import istic.fr.droneproject.service.impl.DronePositionServiceImpl;
+import istic.fr.droneproject.service.impl.DroneServiceImpl;
 import istic.fr.droneproject.service.impl.InterventionServiceCentral;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -22,7 +22,8 @@ public class TestServiceDroneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_service_drone);
-        InterventionServiceCentral.getInstance().getInterventionById("58d1327e9bce7c234254cf28", new Callback<Intervention>() {
+        testrecupDrone();
+        /*InterventionServiceCentral.getInstance().getInterventionById("58d1327e9bce7c234254cf28", new Callback<Intervention>() {
 
             @Override
             public void onResponse(Call<Intervention> call, Response<Intervention> response) {
@@ -33,7 +34,7 @@ public class TestServiceDroneActivity extends AppCompatActivity {
             public void onFailure(Call<Intervention> call, Throwable t) {
                 Log.e("Position Not retreived", "");
             }
-        });
+        });*/
 
     }
 
@@ -47,7 +48,7 @@ public class TestServiceDroneActivity extends AppCompatActivity {
         Cela permet d'appeler un service et dans le callback, de modifier l'interface graphique avec le résultat de l'appel
          */
 
-      /*  DroneService service = new DronePositionServiceImpl();
+      /*  DroneService service = new DroneServiceImpl();
         service.getDrone(new Callback<Drone>() {
             @Override
             public void onResponse(Call<Drone> call, Response<Drone> response) {
@@ -104,20 +105,41 @@ public class TestServiceDroneActivity extends AppCompatActivity {
             });
         }
 
+    public void testAjoutDrone(){
+        Drone drone=new Drone("58d1327e9bce7c234254cf28", EtatDrone.SEGMENT);
+       DroneServiceImpl.getInstance().setDrone(drone, new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.e("Drone added", String.valueOf(response.body()));
+            }
 
-    public void uti(){
-           DroneService service = new DronePositionServiceImpl();
-        service.getDrone(new Callback<Drone>() {
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("Drone not created", "");
+            }
+        });
+
+    }
+
+    public void testrecupDrone(){
+
+        DroneServiceImpl.getInstance().getDroneById("58d1327e9bce7c234254cf28",new Callback<Drone>() {
             @Override
             public void onResponse(Call<Drone> call, Response<Drone> response) {
-                Log.e("La position est ==",response.body().getPosition().get(0)+""+response.body().getPosition().get(1));
+                Log.e("Drone retreived", String.valueOf(response.body()));
             }
 
             @Override
             public void onFailure(Call<Drone> call, Throwable t) {
-                Log.e("Position Not retreived","Drone");
+                Log.e("Drone not created", "");
             }
         });
+
     }
+
+
+
+
+
     }
 
