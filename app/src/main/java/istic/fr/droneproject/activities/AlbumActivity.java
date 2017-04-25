49a9c5d4b1dc99ec6f7a1e63a857f4c4df632d30
bouluad <med.bouluad@gmail.com>
2017-04-25 15:33:43 +0200

    get intervention positions

--- app/src/main/java/istic/fr/droneproject/activities/AlbumActivity.java
+++ app/src/main/java/istic/fr/droneproject/activities/AlbumActivity.java
@@ -1,7 +1,9 @@
 package istic.fr.droneproject.activities;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,15 +22,29 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 //import org.alfonz.utility.VersionUtility;
 
+import java.util.Collections;
+
 import istic.fr.droneproject.R;
+import istic.fr.droneproject.model.EtatVehicule;
+import istic.fr.droneproject.model.Intervention;
+import istic.fr.droneproject.model.Vehicule;
+import istic.fr.droneproject.service.impl.InterventionServiceCentral;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by bouluad on 22/03/17.
  */
 public class AlbumActivity extends Fragment
 {
+
+    private static final String ARG_ID = "idIntervention";
     private View mRootView;
     private MapView mMapView;
+    private String idIntervention;
+    private Intervention intervention;
+    private GoogleMap map;
 
 
     @Override
@@ -46,8 +62,13 @@ public class AlbumActivity extends Fragment
     public void onActivityCreated(Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
+        if (getArguments() != null) {
+            idIntervention = getArguments().getString(ARG_ID);
+
+        }
         setupMap();
         bindData();
+        System.out.println("ID INTERVENTION : "+idIntervention);
     }
 
 
@@ -150,27 +171,54 @@ public class AlbumActivity extends Fragment
     private void setupMap()
     {
         // reference
-        GoogleMap map = ((MapView) mRootView.findViewById(R.id.photo_map)).getMap();
+        map = ((MapView) mRootView.findViewById(R.id.photo_map)).getMap();
 
         // settings
         if(map != null)
         {
-            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            map.setMyLocationEnabled(true);
 
-            UiSettings settings = map.getUiSettings();
-            settings.setAllGesturesEnabled(true);
-            settings.setMyLocationButtonEnabled(true);
-            settings.setZoomControlsEnabled(true);
 
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(new LatLng(49.194696, 16.608595))
-                    .zoom(11)
-                    .bearing(0)
-                    .tilt(30)
-                    .build();
-            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            InterventionServiceCentral.getInstance().getInterventionById(idIntervention, new Callback<Intervention>() {
+                @Override
+                public void onResponse(Call<Intervention> call, Response<Intervention> response) {
+                    intervention = response.body();
+
+                    map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    map.setMyLocationEnabled(true);
+
+                    UiSettings settings = map.getUiSettings();
+                    settings.setAllGesturesEnabled(true);
+                    settings.setMyLocationButtonEnabled(true);
+                    settings.setZoomControlsEnabled(true);
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(new LatLng(intervention.position[0], intervention.position[1]))
+                            .zoom(18)
+                            .bearing(0)
+                            .tilt(30)
+                            .build();
+                    map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                    System.out.println("POSITION INTERVENTION : "+intervention.position[0]+" ; "+intervention.position[1]);
+
+                }
+
+                @Override
+                public void onFailure(Call<Intervention> call, Throwable t) {
+                    //DO NOTHING
+                    Log.e("AlbumActivity", t.toString());
+                }
+            });
+
         }
+    }
+
+    public static AlbumActivity newInstance(String idIntervention) {
+        AlbumActivity fragment = new AlbumActivity();
+        Bundle args = new Bundle();
+        args.putString(ARG_ID, idIntervention);
+        fragment.setArguments(args);
+        return fragment;
     }
 
 }
