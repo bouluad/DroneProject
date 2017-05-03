7c581ee1fd25bea10d7f39c3f6767192b33a093e
Guillaume Chopin <guillaume.chopin@etudiant.univ-rennes1.fr>
2017-05-03 10:27:36 +0200

    Debug : les vehicules parking n'apparaissent plus sur la carte

--- app/src/main/java/istic/fr/droneproject/activities/MapActivity.java
+++ app/src/main/java/istic/fr/droneproject/activities/MapActivity.java
@@ -869,8 +869,8 @@ public class MapActivity extends Fragment implements OnMapReadyCallback {
                 for (int i = 0; i < response.body().vehicules.size(); i++) {
                     Vehicule vehiculeCourant = response.body().vehicules.get(i);
                     if (
-                            (vehiculeCourant.etat == EtatVehicule.PARKING) || (vehiculeCourant.etat == EtatVehicule.DEMANDE || vehiculeCourant.etat == EtatVehicule.ENGAGE) && ((vehiculeCourant.position == null) || (vehiculeCourant.position != null && vehiculeCourant.position[0] == null && vehiculeCourant.position[1] == null))) {
-                        ajoutImageFromVehicule(vehiculesCarte.get(i), i);
+                            vehiculeCourant.etat == EtatVehicule.PARKING ||
+                                    (vehiculeCourant.etat == EtatVehicule.DEMANDE && !Vehicule.positionValide(vehiculeCourant.position))) {
                         vehicules.add(response.body().vehicules.get(i));
                     }
                 }
